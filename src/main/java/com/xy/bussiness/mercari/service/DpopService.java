377cda7133a5a774a4c1d6f6873c60c9a5cd7445
xiayun <11111111>
2023-11-09 15:24:44 +0800

    升级chrome119

--- src/main/java/com/xy/bussiness/mercari/service/DpopService.java
+++ src/main/java/com/xy/bussiness/mercari/service/DpopService.java
@@ -1,34 +1,26 @@
 package com.xy.bussiness.mercari.service;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.google.common.collect.ImmutableList;
 import com.xy.bussiness.mercari.MercariCrawler;
-import com.xy.bussiness.mercari.mybean.MercariSearchCondition;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v112.network.Network;
-import org.openqa.selenium.devtools.v112.network.model.Request;
+import org.openqa.selenium.devtools.v119.network.Network;
+import org.openqa.selenium.devtools.v119.network.model.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Service
@@ -63,8 +55,8 @@ public class DpopService {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", driverAddress);
+//        ChromeDriver chromeDriver = new ChromeDriver(options);
         ChromeDriver chromeDriver = new ChromeDriver(options);
-
         DevTools chromeDevTools = chromeDriver.getDevTools();
         chromeDevTools.createSession();
 
